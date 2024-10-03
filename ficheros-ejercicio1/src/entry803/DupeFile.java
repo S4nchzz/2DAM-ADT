4b1389af4f2a53b5141ce2b190371070edc5f4dc
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2024-10-03 17:51:10 +0200

    Exercise 803 finalized

---
+++ ficheros-ejercicio1/src/entry803/DupeFile.java
@@ -0,0 +1,41 @@
+package entry803;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DupeFile {
+    public static void main(String[] args) {
+        dupeFile();    
+    }
+
+    private static void dupeFile() {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Escribe la ruta del archivo que quiere copiar: ");
+        final String fromFile = sc.next();
+
+        System.out.println("Escribe la ruta del archivo donde quiere pegar el contenido: ");
+        final String toFile= sc.next();
+
+        try {
+            File fileA = new File(fromFile);
+            File fileB = new File(toFile);
+            
+            FileReader reader = new FileReader(fileA);
+            FileWriter writer = new FileWriter(fileB);
+
+            int value;
+            while ((value = reader.read()) != -1) {
+                writer.write(value);
+            }
+
+            reader.close();
+            writer.close();
+        } catch (IOException ioe) {
+
+        }
+    }
+}
